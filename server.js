5a62c01fdaf28124e0b3d8f2003612e9910be2f2
Bogdan Myagkov <bboddya2000@mail.ru>
2020-11-10 18:54:21 +0300

    add

---
+++ server.js
@@ -0,0 +1,10 @@
+const static = require('node-static')
+const http = require('http')
+const file = new static.Server('./public')
+const server = http.createServer((req, res) => {
+    req.addListener('end', () => {
+        file.serve(req, res)
+    }).resume()
+})
+server.listen(process.env.PORT || 3000)
+console.log('Server started')
